8faccaf2e4732cdb03287ba3424f9ba5904fbf7f
Shin Le <syl21b@gmail.com>
2025-12-25 17:42:05 -0500

    Update with all enhancements

--- app.py
+++ app.py
@@ -2590,7 +2590,5 @@ if __name__ == '__main__':
             import sys
             sys.exit(1)
     
-    port = int(os.environ.get('PORT', 3000))
-    debug_mode = not bool(os.environ.get('RENDER'))
-    
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    port = int(os.environ.get("PORT", 5003)) 
+    app.run(host="0.0.0.0", port=port)
